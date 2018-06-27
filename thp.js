ee8c51dd3957ff25c0408f71d9f0982c94a320dc
Romain <romainsaillour@orange.fr>
2018-06-28 00:04:24 +0200

    modif RS

--- thp.js
+++ thp.js
@@ -1,45 +1,10 @@
-var r = document.getElementById('rbw');
-var currentHue = 0;
-var hueAddition = 5;
-function doThatFuckingColorThing()
-{
-  var color = 'hsl(' + currentHue + ',80%, 60%)';
-  if(currentHue + hueAddition > 360)
-  {
-    currentHue = 0;
-  }
-  else {
-    currentHue += hueAddition;
-  }
-  r.style.color = color;
-  setTimeout(function(){doThatFuckingColorThing();}, 1000/25);
-}
-
-function someControl(id, textArr, className){
-  // You see? No fucking jQuery needed, check http://jsperf.com/getelementbyid-vs-jquery-id/44
-  var acbox = document.getElementById(id);
-  var clickEvent = ('ontouchstart' in window ? 'touchend' : 'click');
-  var el = document.getElementsByTagName('html')[0];
-  acbox.addEventListener(clickEvent, function(){
-    if(acbox.innerHTML === textArr[0]){
-      el.classList.add(className);
-      acbox.innerHTML = textArr[1];
-    }
-    else{
-      el.classList.remove(className);
-      acbox.innerHTML = textArr[0];
-    }
-  });
-}
-
-function addContrastControl(){
-  someControl('contrast', ['Add more contrast', 'Remove additional contrast'], 'contrast');
-}
+var magenta = document.querySelector('#magenta');
+            
+            magenta.addEventListener('click', () => {
+                document.body.style.backgroundColor = 'magenta';
+                document.body.style.color = 'blue'
+                                 });
 
 function addInvertedControl(){
   someControl('invmode', ['Inverted mode', 'Normal mode'], 'inverted');
 }
-
-doThatFuckingColorThing();
-addContrastControl();
-addInvertedControl();
