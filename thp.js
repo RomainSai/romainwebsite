2d4130db06de261b95c3c009330e802f3842a1a6
Romain SLR <37241638+RomainSai@users.noreply.github.com>
2018-06-29 11:59:22 +0200

    Projet finit

--- thp.js
+++ thp.js
@@ -1,10 +1,12 @@
+function PopUp(){
+        document.getElementById('popup1').style.display="none"; 
+}
+
+setTimeout(function Alert() { alert("Ce site en vaut vraiment la peine ..."); }, 10000);
+
 var magenta = document.querySelector('#magenta');
             
             magenta.addEventListener('click', () => {
                 document.body.style.backgroundColor = 'magenta';
                 document.body.style.color = 'blue'
                                  });
-
-function addInvertedControl(){
-  someControl('invmode', ['Inverted mode', 'Normal mode'], 'inverted');
-}
